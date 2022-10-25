9aec9e4a5e8d1c2df46e4a3a29204f2ed9ca3e59
Adam Salim <adamsaleem270@gmail.com>
2022-10-25 01:45:30 -0700

    More linked list

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -2,7 +2,7 @@
 
 /**
  * add_nodeint_end - Adds a new node at the
- * 	             end of a listint_t list.
+ *                   end of a listint_t list.
  * @head: A pointer to the address of the
  *        head of the listint_t list.
  * @n: The integer for the new node to contain.
