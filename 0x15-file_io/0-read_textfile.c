d42ba96f5ac13fa3ab5a7fbc01d6f2c46058e347
Adam Salim <adamsaleem270@gmail.com>
2022-10-31 23:47:57 -0700

    file dscription

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -8,7 +8,7 @@
  *           function should read and print.
  *
  * Return: If the function fails or filename is NULL - 0.
- *          O/w - the actual number of bytes the function can read and print.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
